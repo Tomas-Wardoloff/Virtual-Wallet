eb52fd8680943397248521748c9a2d40c9085546
Tomas Wardoloff <tomaswardoloff@gmail.com>
2023-05-09 22:32:44 -0300

    Function to enter a valid currency

--- src/check_input.py
+++ src/check_input.py
@@ -17,15 +17,17 @@ Restriction:
 The user name must be between 6 and 50 characters long 
 """
 def check_user_name(message: str) -> str:
-    user_input = input(f"\n• Must be between 6 and 50 characters long\n{message}").strip()
+    user_input = input(
+        f"\n• Must be between 6 and 50 characters long\n{message}"
+    ).strip()
     while True:
         if 3 < len(user_input) < 50:
-            return user_input
+            return user_input.capitalize()
         print("Must be between six and 50 characters long")
-        user_input = input(message)
+        user_input = input(message).strip()
 
 
-"""This function iterate until the user input a valid email"""
+"""This function iterate until the user enter a valid email"""
 def check_email() -> str:
     user_input = input("\n• your_name@example.com\nInsert your email: ").strip()
     while True:
@@ -34,3 +36,31 @@ def check_email() -> str:
         else:
             print("Invalid input. Try again")
             user_input = input("Insert your email: ").strip()
+
+
+"""This function show all the currencies available and iterate until the user enter a valid option"""
+def check_currency() -> str:
+    currencies = {
+        1: "United States Dollar",
+        2: "Euro",
+        3: "Japanese Yen",
+        4: "British Pound",
+        5: "Australian Dollar",
+        6: "Canadian Dollar",
+        7: "Swiss Franc",
+        8: "Chinese Yuan",
+        9: "Hong Kong Dollar",
+        10: "Swedish Krona",
+    }
+    for index in range(1,11):
+        print(index, currencies[index])
+        
+    while True:
+        try:
+            option = int(input("Enter the currency of your choice: "))
+            if option in list(range(1,11)):
+                return currencies[option]
+            else:
+                print("Option out of range")
+        except ValueError:
+            print("Invalid input. Try again")
